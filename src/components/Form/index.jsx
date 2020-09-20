329c481182e58c740e85cceace72daafbcc38d2f
sadia <sadia@techhivebd.com>
2020-09-20 23:07:13 +0600

    add formik

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,16 +1,33 @@
 import React, { useEffect, useState } from 'react'
-import {getForm} from '../../services/GetService';
+import { getForm } from '../../services/GetService';
+
+
+const initialValues = {};
+
 
 function Form({ type }) {
+  const [formList, setFormDetails] = useState([])
   useEffect(() => {
-    getForm().then(res=>{
-console.log(res)
-    })
-  })
+    getForm().then(response => {
+      const list = response.data.fields;
+      setFormDetails(list[0])
+      Object.keys(list[0]).map(function (key, index) {
+        initialValues[key] = '';
+      })
+      console.log(initialValues);
+    });
+  }, [])
+
+
   return (
     <div>
       {type === 'create' && <h1>create new</h1>}
       {type === 'update' && <h1>update</h1>}
+      <form onSubmit={formik.handleSubmit}>
+        <div className="form-group">
+
+        </div>
+      </form>
     </div>
   )
 }
--- src/services/GetService.js
+++ src/services/GetService.js
@@ -2,6 +2,6 @@
 import axios from 'axios';
 
 export function getForm() {
-  return axios.get(`/api/get_form.php`).then(response => response);
+  return axios.get(`/api/get_form.php`).then(response => response.data);
 }
 
